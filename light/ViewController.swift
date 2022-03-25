c30990e682407087ddc4777ed68c211bf80b3125
Alexander Gnilitskiy <gnilitskiy1997@icloud.com>
2022-03-25 23:08:31 +0300

    OHHH

--- light/ViewController.swift
+++ light/ViewController.swift
@@ -8,12 +8,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+   // @IBOutlet var redLightLabel: UILabel!
+   // @IBOutlet var yellowLightLabel: UILabel!
+   // @IBOutlet var greenLightLabel: UILabel!
+   // @IBOutlet var switchLight: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
 
