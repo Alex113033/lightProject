08240746798c86c5dc259bb20e5fa1142464e187
Alexander Gnilitskiy <gnilitskiy1997@icloud.com>
2022-03-26 15:45:01 +0300

    Working

--- light/ViewController.swift
+++ light/ViewController.swift
@@ -9,13 +9,47 @@ import UIKit
 
 class ViewController: UIViewController {
     
-   // @IBOutlet var redLightLabel: UILabel!
-   // @IBOutlet var yellowLightLabel: UILabel!
-   // @IBOutlet var greenLightLabel: UILabel!
-   // @IBOutlet var switchLight: UIButton!
+    @IBOutlet var redLightView: UIView!
+    @IBOutlet var yellowLightView: UIView!
+    @IBOutlet var greenLightView: UIView!
+    
+    @IBOutlet var switchLightButton: UIButton!
+    var tumblerOne = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
+//        redLightView.isHidden = true
+//        yellowLightView.isHidden = true
+//        greenLightView.isHidden = true
+        
+        redLightView.layer.cornerRadius = 60
+        yellowLightView.layer.cornerRadius = 60
+        greenLightView.layer.cornerRadius = 60
+        
+        switchLightButton.layer.cornerRadius = 10
+        
+        redLightView.alpha = 0.3
+        yellowLightView.alpha = 0.3
+        greenLightView.alpha = 0.3
+    }
+    
+   
+    @IBAction func pressingButton() {
+        redLightView.alpha = 0.3
+        yellowLightView.alpha = 0.3
+        greenLightView.alpha = 0.3
+        
+        if tumblerOne == 0 {
+            redLightView.alpha = 1
+            switchLightButton.setTitle("NEXT", for: .normal)
+            tumblerOne = 1
+        } else if tumblerOne == 1 {
+            yellowLightView.alpha = 1
+            tumblerOne = 2
+        } else if tumblerOne == 2 {
+            greenLightView.alpha = 1
+            tumblerOne = 0
+        }
     }
 }
 
